a6cf8d9e62fb8c9db4ba2108c8dfe8f070332aa5
rashids111 <syedarashid25@gmail.com>
2022-07-13 17:27:23 -0400

    July 13

--- src/services/giphy-api.js
+++ src/services/giphy-api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const url = "https://api.giphy.com/v1/gifs"
 
-const api_key = "5lkH9i5monHElq19pfoOQjC0mTweBZGk"
+const api_key = "YOUR API KEY HERE"
 
 export default async function getGifs(query) {
     let full_url = url + "/search?api_key=" + api_key + "&q=" + query + "&limit=20&offset=0&rating=G&lang=en"
